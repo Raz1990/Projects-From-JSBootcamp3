f69356cc0f5b982036a06f931396866d824995d0
Raz <raz2712@gmail.com>
2018-04-23 13:16:02 +0300

    update 1

--- Day 2/Logging/Logging.js
+++ Day 2/Logging/Logging.js
@@ -13,17 +13,8 @@ var Person = {num: 10000, name: 'Raz'};
 //Wrapper
 
 function betterLogging(n1,n2,oper) {
-    if (isNaN(n1) && isNaN(n2)){
-        if(oper === '+')
-        {
-            console.log(n1+n2);
-        }
-        else
-        {
-            console.error('What you wanna do can\'t be done with strings!');
-        }
-    }
-    else {
+    //if the two vars are numbers
+    if (!isNaN(n1) && !isNaN(n2)){
         switch (oper) {
             case '+':
                 console.log(n1+ '+' + n2 + '= %d', n1+n2);
@@ -38,6 +29,22 @@ function betterLogging(n1,n2,oper) {
                 console.warn('I don\'t know what you want me to do... only +, * and - are allowed!');
         }
     }
+    //if the two are strings
+    else if (isNaN(n1) && isNaN(n2)){
+        if(oper === '+')
+        {
+            console.log(n1+n2);
+        }
+        else
+        {
+            console.error('What you wanna do can\'t be done with strings!');
+        }
+
+    }
+    else
+    {
+        console.error('2 different vars detected! Bad user!');
+    }
 }
 
 betterLogging(5,5,'+');
@@ -46,3 +53,4 @@ betterLogging(5,5,'-');
 betterLogging('abc','def','+');
 betterLogging('abc','def','*');
 betterLogging(5,5,'/');
+betterLogging(5,'abc','+');
